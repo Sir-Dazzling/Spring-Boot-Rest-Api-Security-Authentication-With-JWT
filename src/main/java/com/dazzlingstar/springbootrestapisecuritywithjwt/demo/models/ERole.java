bc8e977b62919e304cb8feb0c58f37d59733095e
Otti Daniel <33727291+Sir-Dazzling@users.noreply.github.com>
2020-04-29 23:05:36 +0100

    set up basic spring boot project structure

---
+++ src/main/java/com/dazzlingstar/springbootrestapisecuritywithjwt/demo/models/ERole.java
@@ -0,0 +1,8 @@
+package com.dazzlingstar.springbootrestapisecuritywithjwt.demo.models;
+
+public enum ERole
+{
+    ROLE_USER,
+    ROLE_CONTENT_MANAGER,
+    ROLE_ADMIN
+}
---
+++ src/test/java/com/dazzlingstar/springbootrestapisecuritywithjwt/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dazzlingstar.springbootrestapisecuritywithjwt.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
